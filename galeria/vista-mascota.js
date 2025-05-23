cc99fe63ce0918705af9b89be752ddd9fb210377
Kilerwin <miguel642013@gmail.com>
2025-05-23 06:07:06 -0500

    Se realiza mejora en vista de mascota para cargar los datos desde el localStorage

--- galeria/vista-mascota.js
+++ galeria/vista-mascota.js
@@ -3,6 +3,7 @@ const cargaInicial = () => {
   const nombreMascota = params.get("nombre")
 
   if (nombreMascota) {
+    document.getElementById("nombre-mascota-navegacion").innerHTML = nombreMascota
     const key = "mascotas";
     const mascotas = JSON.parse(localStorage.getItem(key)) || []
     const mascota = mascotas.find(m => m.nombre === nombreMascota)
@@ -20,6 +21,53 @@ const cargaInicial = () => {
       document.getElementById("vacunado").textContent = mascota.vacunado ? "Sí" : "No"
       document.getElementById("castrado").textContent = mascota.castrado ? "Sí" : "No"
       document.getElementById("historial").textContent = mascota.historial
+
+      const carouselInner = document.getElementById("carouselInner");
+      const carouselIndicators = document.getElementById("indicadores");
+      const imagenesCarrucel = document.getElementById("imagenesCarrucel");
+
+
+
+      if (!carouselInner) {
+        console.error("Faltan elementos del DOM (carouselInner, imagenesCarrucel o carouselIndicators)");
+        return;
+      }
+
+      mascota.listaImagenes.forEach((src, index) => {
+        const carouselItem = document.createElement("div");
+        carouselItem.className = "carousel-item" + (index === 0 ? " active" : "");
+
+        const imgBootstrap = document.createElement("img");
+        imgBootstrap.src = src;
+        imgBootstrap.className = "d-block w-100";
+        imgBootstrap.alt = `Imagen ${index + 1}`;
+
+        carouselItem.appendChild(imgBootstrap);
+        carouselInner.appendChild(carouselItem);
+
+        const indicator = document.createElement("button");
+        indicator.type = "button";
+        indicator.setAttribute("data-bs-target", "#carouselExampleIndicators");
+        indicator.setAttribute("data-bs-slide-to", index);
+        indicator.setAttribute("aria-label", `Slide ${index + 1}`);
+        if (index === 0) {
+          indicator.className = "active";
+          indicator.setAttribute("aria-current", "true");
+        }
+        carouselIndicators.appendChild(indicator);
+
+        const imagenDiv = document.createElement("div");
+        imagenDiv.className = "imagen";
+
+        const imgCustom = document.createElement("img");
+        imgCustom.src = src;
+        imgCustom.className = "d-block w-100";
+        imgCustom.alt = `Imagen ${index + 1}`;
+
+        imagenDiv.appendChild(imgCustom);
+        imagenesCarrucel.appendChild(imagenDiv);
+      });
+
     } else {
       document.querySelector(".informacion").innerHTML = "<p>Mascota no encontrada.</p>"
     }
@@ -29,8 +77,12 @@ const cargaInicial = () => {
 }
 
 const mascotasSimilares = () => {
+  const params = new URLSearchParams(window.location.search)
+  const nombreMascota = params.get("nombre")
   const key = "mascotas"
-  const mascotas = JSON.parse(localStorage.getItem(key)) || []
+  let mascotas = JSON.parse(localStorage.getItem(key)) || []
+  mascotas = mascotas.filter(objeto => objeto.nombre !== nombreMascota);
+
 
   if (mascotas.length === 0) return
 
@@ -50,7 +102,7 @@ const mascotasSimilares = () => {
     contenedor.classList.add("grid-item")
 
     const imagen = document.createElement("img")
-    imagen.src = "../images/testimonio5.jpg"
+    imagen.src = mascota.imagen || "../images/testimonio5.jpg";
     imagen.alt = mascota.nombre
     imagen.classList.add("item-img")
 
